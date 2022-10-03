ce9971cbafa67fd9628a727da498d8e53f472478
gursewak <gursewak@gursewaks-MacBook-Pro.local>
2022-10-03 21:19:03 +0530

    new pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,11 +19,15 @@ module.exports = {
                 "light-100": "#FCFCFC",
                 "light-200": "#F2F2F2",
                 "light-300": "#DBDBDB",
+                "dark-800": "#666666",
                 "dark-700": "#333333",
                 "dark-600": "#4D4D4D",
+                "dark-500": "#808080",
+
             },
             boxShadow: {
-                'box': '0px 4px 24px rgba(0, 0, 0, 0.05);',
+                'box': '0px 4px 24px rgba(0, 0, 0, 0.05)',
+                'select': '0px 1px 20px rgba(0, 0, 0, 0.1)'
             },
             backgroundImage: {
                 'start-bg': "url('/assets/images/start-bg.svg')",
