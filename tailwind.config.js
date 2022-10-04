8cf94baa0cef7f2a56193de11209f3167549e572
gursewak <gursewak@gursewaks-MacBook-Pro.local>
2022-10-04 21:52:18 +0530

    new registration screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
                 "primary-700": "#006D9E",
                 "secondary-900": "#81DBDF",
                 "secondary-700": "#E7F8F8",
+                "secondary-100": "#CAF0F2",
                 "light-100": "#FCFCFC",
                 "light-200": "#F2F2F2",
                 "light-300": "#DBDBDB",
@@ -23,7 +24,8 @@ module.exports = {
                 "dark-700": "#333333",
                 "dark-600": "#4D4D4D",
                 "dark-500": "#808080",
-
+                "success": "#4FCB14",
+                "danger": "#FF1111",
             },
             boxShadow: {
                 'box': '0px 4px 24px rgba(0, 0, 0, 0.05)',
